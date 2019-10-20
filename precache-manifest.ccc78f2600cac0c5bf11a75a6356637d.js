6a2f6f4bd61d828b41d5742bcdc18b49492314e9
zikwall <andrey.kapitonov.96@gmail.com>
2019-10-20 16:57:00 +0300

    Updates

---
+++ precache-manifest.ccc78f2600cac0c5bf11a75a6356637d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81f49e9a33494915e37fa6ebaafc72f",
+    "url": "/find-you/index.html"
+  },
+  {
+    "revision": "814e762029f71fc9ec95",
+    "url": "/find-you/static/css/2.7214840c.chunk.css"
+  },
+  {
+    "revision": "814e762029f71fc9ec95",
+    "url": "/find-you/static/js/2.a94f9295.chunk.js"
+  },
+  {
+    "revision": "274168ab0e0c2f81d9f7",
+    "url": "/find-you/static/js/main.2e9022fd.chunk.js"
+  },
+  {
+    "revision": "d68b83beee6e1dfd9d87",
+    "url": "/find-you/static/js/runtime-main.b17aacb6.js"
+  }
+]);
