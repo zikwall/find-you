333d113bdeedcbdfbe5a87a57c2bb30c705eb20a
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-10-24 11:20:51 +0300

    :art: Add version control for cache

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
                     <PanelHeader>Ищу тебя <Heart /></PanelHeader>
                     <Main/>
 
-                    <Footer> status:@dev </Footer>
+                    <Footer> status:@dev-v1.1.2 </Footer>
                 </Panel>
             </View>
         </Root>
